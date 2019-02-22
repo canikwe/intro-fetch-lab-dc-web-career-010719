83e0601d8795cf344a6ddb2edf953043a4873363
canikwe <c.anikwe@gmail.com>
2019-02-22 12:15:05 -0500

    Set Start Wars opening Event Listeners

--- swapi.js
+++ swapi.js
@@ -1,1 +1,37 @@
-// Write your swapi code in this file!
+//set variables and function for crawl event listener
+let crawlDiv = document.querySelector("#crawlDiv")
+let crawlBtn = document.querySelector("#crawlBtn")
+
+
+function getOpeningCrawl() {
+  fetch('https://swapi.co/api/films/1/')
+  .then(res => res.json())
+  .then(json => crawlDiv.innerText = json.opening_crawl)
+}
+//set crawl event listener
+crawlBtn.addEventListener("click", getOpeningCrawl)
+
+//set variables and function for getPlanet
+let findPlanet = document.querySelector("#findPlanet")
+let pDataDiv = document.querySelector("#planetData")
+
+function getPlanet(e) {
+  e.preventDefault()
+  
+  let planetInput = document.querySelector("#planetInput").value
+  fetch(`https://swapi.co/api/planets/${planetInput}`)
+  .then(res => {
+    debugger
+    res.json()})
+  .then(json => planetData(json))
+  
+}
+
+function planetData(json) {
+  
+  pDataDiv.innerHTML = `<p>Name: ${json.name}</p><p>Climate: ${json.climate}</p>`
+  
+}
+
+//set getPlanet event listener
+findPlanet.addEventListener("click", getPlanet)
